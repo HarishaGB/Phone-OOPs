5002d523bd42581598287ec57b8be5e1090032f7
HarishaGB <harshagb555@gmail.com>
2025-07-17 00:39:15 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        LandLine l1 = new LandLine("123");
+        LandLine l2 = new LandLine("456");
+
+        l1.callNumber("456");
+        l2.receiveCall("123");
+        System.out.println(l2.answerCall());
+    }
+}
